9a14c0f26e835c1a19a50e302928ed95c13441a1
Bala <balakumaran.kannan@microsoft.com>
2024-07-21 14:40:53 +0530

    Handle Moves and Promotions

--- web/static/shan_chess.js
+++ web/static/shan_chess.js
@@ -1,5 +1,7 @@
-import {Chessboard, FEN, INPUT_EVENT_TYPE} from './node_modules/cm-chessboard/src/Chessboard.js';
+import {Chessboard, COLOR, INPUT_EVENT_TYPE} from './node_modules/cm-chessboard/src/Chessboard.js';
 import {Markers} from './node_modules/cm-chessboard/src/extensions/markers/Markers.js';
+import {PROMOTION_DIALOG_RESULT_TYPE, PromotionDialog} from "./node_modules/cm-chessboard/src/extensions/promotion-dialog/PromotionDialog.js"
+import {Accessibility} from "./node_modules/cm-chessboard/src/extensions/accessibility/Accessibility.js";
 import {Chess} from './chess.js';
 
 
@@ -7,7 +9,7 @@ function get_puzzle() {
     return {
         "fen": "q3k1nr/1pp1nQpp/3p4/1P2p3/4P3/B1PP1b2/B5PP/5K2 b k - 0 17",
         "solution": "e8",
-        "orientation": "white"
+        "color": COLOR.white
     }
 }
 
@@ -15,75 +17,69 @@ let puzzle = get_puzzle();
 let chess = new Chess(
     puzzle.fen
 );
-let piece = null
-let color = null
-let start_square = null
-let end_square = null
-let move = null;
 
 const board = new Chessboard(document.getElementById('board'), {
     assetsUrl: "./node_modules/cm-chessboard/assets/",
     position: puzzle.fen,
     style: {pieces: {file: "pieces/staunty.svg"}},
-    extensions: [{class: Markers}]
+    orientation: puzzle.color,
+    extensions: [
+        {class: Markers},
+        {class: PromotionDialog},
+        {class: Accessibility, props: {visuallyHidden: true}}
+    ]
 });
 
-board.enableMoveInput((event) => {
+function input_handler(event) {
     console.log(event);
     switch (event.type) {
         case INPUT_EVENT_TYPE.moveInputStarted:
-            color = get_color(event.piece);
-            piece = get_piece(event.piece);
-            start_square = event.square;
             return true;
+
+        case INPUT_EVENT_TYPE.validateMoveInput:
+            const move = {from: event.squareFrom, to: event.squareTo, promotion: event.promotion};
+            const move_result = chess.move(move);
+
+            if (move_result) {
+                event.chessboard.state.moveInputProcess.then(() => {
+                    event.chessboard.setPosition(chess.fen(), true);
+                });
+            }
+            else {
+                let possible_moves = chess.moves({square: event.squareFrom, verbose: true});
+                for (const possible_move of possible_moves) {
+                    if (possible_move.promotion && possible_move.to === event.squareTo) {
+                        event.chessboard.showPromotionDialog(event.squareTo, puzzle.color, (result) => {
+                            console.log("promotion result: ", result);
+                            if (result.type === PROMOTION_DIALOG_RESULT_TYPE.pieceSelected) {
+                                chess.move({from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1)});
+                                event.chessboard.setPosition(chess.fen(), true);
+                            }
+                            else {
+                                event.chessboard.enableMoveInput(input_handler, puzzle.color);
+                                event.chessboard.setPosition(chess.fen(), true);
+                            }
+                        });
+                        return true;
+                    }
+                }
+            }
+            return move_result;
 
         case INPUT_EVENT_TYPE.moveInputFinished:
-            if (piece == null) {
-                // Cancelled Move
-                return true;
-            }
-            if (start_square != event.squareFrom) {
-                console.log("Didn't capture the starting square properly!\nSomething is wrong in the code.");
-                Exception("Didn't capture the starting squre properly!\nSomething is wrong in the code.");
-            }
-            end_square = event.squareTo;
-            move = construct_move(start_square, end_square, piece, color);
-            validate(move, puzzle);
-            return true;
-
-        case INPUT_EVENT_TYPE.moveInputCanceled:
-            piece = color = null;
-            start_square = null;
-            end_square = null;
+            update_and_validate(event, puzzle);
             return true;
 
         default:
             return true;
     }
-});
-
-function get_color(piece) {
-    return piece[0].toLowerCase();
 }
 
-function get_piece(piece) {
-    piece = piece[1].toLowerCase();
-    if (piece == "p") {
-        return "";
+function update_and_validate(event, puzzle) {
+    if (event.legalMove == null) {
+        alert("Illegal Move!");
     }
-    return piece.toUpperCase();
+   return 0;
 }
 
-function construct_move(start_square, end_square, piece, color) {
-    // TODO: Handle Castling, En passant, Promotion
-    // TODO: Handle when two pieces of same type can move to the same square
-    return piece + end_square;
-}
-
-function validate(move, puzzle) {
-    if (move == puzzle.solution) {
-        alert("Congratulations! You solved the puzzle!");
-    } else {
-        alert("Oops! That's not the right move. Try again!");
-    }
-}
+board.enableMoveInput(input_handler);
